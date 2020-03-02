659e593f4be7d078b4210571e597864c14ee6aa7
hjs557523 <326464623@qq.com>
2020-03-02 19:27:38 +0800

    框架搭建

---
+++ src/test/java/com/hjs/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjs.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
