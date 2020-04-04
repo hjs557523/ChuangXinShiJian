bfbdcc08517be3f9480f84fc2f16bcbf6ae82b70
hjs557523 <326464623@qq.com>
2020-04-04 13:30:00 +0800

    修改项目启动banner

--- src/main/java/com/hjs/system/SystemApplication.java
+++ src/main/java/com/hjs/system/SystemApplication.java
@@ -2,6 +2,7 @@ package com.hjs.system;
 
 //import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,15 +22,16 @@ public class SystemApplication {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境机器各 Spring 组件
         SpringApplication.run(SystemApplication.class, args);
-        System.out.println("*****************************************************************************************************");
-        System.out.println("***                                Hjs's system start-up success                                  ***");
-        System.out.println("*****************************************************************************************************");
-        System.out.println("***                       ChuangXinShiJian Course Management System v1.0                          ***");
-        System.out.println("*****************************************************************************************************");
-        System.out.println("***       Note: The System supports WeChat applet and web client-side, multi - terminal use       ***");
-        System.out.println("*****************************************************************************************************");
-        System.out.println("***                       Author: Huang JiSheng    Finish Time: 2020/5/1                          ***");
-        System.out.println("*****************************************************************************************************");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t*********************************************************************************************************");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t**                                   Hjs's system start-up success                                     **");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t*********************************************************************************************************");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t**                          ChuangXinShiJian Course Management System v1.0                             **");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t*********************************************************************************************************");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t**          Note: The System supports WeChat applet and web client-side, multi - terminal use          **");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t*********************************************************************************************************");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t**                          Author: Huang JiSheng    Finish Time: 2020/5/1                             **");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t*********************************************************************************************************");
+
     }
 
 }
